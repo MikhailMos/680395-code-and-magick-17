1d9129adeb090a7467ac29162b0c6d046770e350
MikhailMos <mos-mv@mail.ru>
2019-07-08 02:11:10 +0300

    Выполняет задание 15.1

--- js/colorize.js
+++ js/colorize.js
@@ -1,16 +1,24 @@
 'use strict';
 
 (function () {
+
   window.corolize = function (element, colors, inputDefaultValue) {
-    element.addEventListener('click', function () {
+    element.addEventListener('click', function (evt) {
       var color = colors[window.utils.getRandomInt(0, colors.length)];
       if (element.tagName.toLowerCase() === 'div') {
         inputDefaultValue = color;
         element.style.backgroundColor = inputDefaultValue;
+        window.setup.colorFireball = inputDefaultValue;
       } else {
         element.style.fill = color;
         inputDefaultValue = element.style.fill;
+        if (evt.target.classList.contains('wizard-eyes')) {
+          window.setup.colorEyes = color;
+        } else {
+          window.setup.colorCoat = color;
+        }
       }
+      window.setup.updateWizards();
     });
   };
 })();
---
+++ js/render.js
@@ -0,0 +1,38 @@
+'use strict';
+
+(function () {
+  var MAX_WIZARD = 5;
+
+  var renderWizard = function (wizard) {
+    var wizardElement = similarWizardTemplate.cloneNode(true);
+
+    wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
+    // wizardElement.querySelector('.setup-fireball-wrap').style.backgroundColor = wizard.colorFireball;
+
+    return wizardElement;
+  };
+
+  window.render = function (data) {
+    var fragment = document.createDocumentFragment();
+    var takeNumber = data.length > MAX_WIZARD ? MAX_WIZARD : data.length;
+
+    similarListElement.innerHTML = '';
+
+    for (var i = 0; i < takeNumber; i++) {
+      fragment.appendChild(renderWizard(data[i]));
+      // similarListElement.appendChild(renderWizard(data[i]));
+    }
+
+    similarListElement.appendChild(fragment);
+    if (similar.classList.contains('hidden')) {
+      similar.classList.remove('hidden');
+    }
+  };
+
+  var similar = document.querySelector('.setup-similar');
+  var similarListElement = document.querySelector('.setup-similar-list');
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+
+})();
